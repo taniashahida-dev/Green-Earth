daeddbd5d45441971e2ea1d91fd9ad11be5ed473
taniashahida-dev <taniashahida993@gmail.com>
2026-03-05 17:02:44 +0600

    add some funtionality in buttons

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
-const catagoriesContainer = document.getElementById('Categories-container')
-const plantsContainer = document.getElementById('plants-container')
+const catagoriesContainer = document.getElementById("Categories-container");
+const plantsContainer = document.getElementById("plants-container");
+const loadingSpinner = document.getElementById("loading-spinner");
 
+const showLoading = () => {
+  loadingSpinner.classList.remove("hidden");
+  plantsContainer.innerHTML = "";
+};
+const hideLoading = () => {
+  loadingSpinner.classList.add("hidden");
+};
 
-const catagoriesBth = ()=>{
-    const url = 'https://openapi.programming-hero.com/api/categories'
-    fetch(url)
-    .then((res)=> res.json())
-    .then((data)=> displayCatagoriesBth(data.categories))
-    .catch((e)=>console.log(e));
+const catagoriesBth = () => {
+  const url = "https://openapi.programming-hero.com/api/categories";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCatagoriesBth(data.categories))
+    .catch((e) => console.log(e));
+};
+const displayCatagoriesBth = (buttons) => {
+  catagoriesContainer.innerHTML = "";
+  buttons.forEach((button) => {
+    const btn = document.createElement("div");
+    btn.innerHTML = `
+     <button class="btn  w-full category-btn" onclick="selectCategory(${button.id})">${button.category_name}</button>
+    `;
+    catagoriesContainer.appendChild(btn);
+  });
+};
 
-}
-const displayCatagoriesBth =(buttons)=>{
-catagoriesContainer.innerHTML = ""
-buttons.forEach(button => {
-    const div = document.createElement("div")
+const selectCategory = (categoryId) => {
+showLoading()
+  const allCategoriesBtn = document.querySelectorAll(
+    "#Categories-container button, #trees-btn"
+  );
+// console.log(allCategoriesBtn)
+  allCategoriesBtn.forEach((button) => {
+    button.classList.remove("text-white", "bg-green-400");
+  });
+
+  const clickedBtn = event.currentTarget;
+  clickedBtn.classList.add("text-white", "bg-green-400");
+  console.log(clickedBtn)
+};
+
+const loadPlant = () => {
+  showLoading();
+  const url = "https://openapi.programming-hero.com/api/plants";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displayPlant(data.plants);
+      hideLoading();
+    });
+};
+const displayPlant = (allplants) => {
+  plantsContainer.innerHTML = "";
+
+  allplants.forEach((plant) => {
+    const div = document.createElement("div");
+    div.className = "card bg-base-100 shadow-sm";
     div.innerHTML = `
-     <button class="btn  w-full">${button.category_name}</button>
-    `
-    catagoriesContainer.appendChild(div)
-});
-}
-
-
-const loadPlant = ()=>{
-    const url = 'https://openapi.programming-hero.com/api/plants'
-    fetch(url)
-    .then((res)=>res.json())
-    .then((data)=>displayPlant(data.plants))
-}
-const displayPlant=(allplants)=>{
-plantsContainer.innerHTML=""
-
-allplants.forEach(plant=>{
-const div = document.createElement('div')
-div.className = "card bg-base-100 shadow-sm"
-div.innerHTML= `
  
   <figure>
     <img
      src="${plant.image}"
+     alt="${plant.name}"
+     title="${plant.name}"
+     class="h-48 w-full object-cover"
        />
   </figure>
   <div class="card-body">
@@ -50,10 +78,9 @@ div.innerHTML= `
     </div>
   </div>
 
-`
-plantsContainer.appendChild(div)
-})
-
-}
-catagoriesBth()
-loadPlant()
+`;
+    plantsContainer.appendChild(div);
+  });
+};
+catagoriesBth();
+loadPlant();
