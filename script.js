61897103a57cdfea849f49ce0d661a8c377a69b1
taniashahida-dev <taniashahida993@gmail.com>
2026-03-06 14:13:33 +0600

    Add modal dynamically

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const catagoriesContainer = document.getElementById("Categories-container");
 const plantsContainer = document.getElementById("plants-container");
 const loadingSpinner = document.getElementById("loading-spinner");
+const modalContainer = document.getElementById('modal-container')
+// const modal = document.getElementById('modal')
 
 const showLoading = () => {
   loadingSpinner.classList.remove("hidden");
@@ -9,6 +11,28 @@ const showLoading = () => {
 const hideLoading = () => {
   loadingSpinner.classList.add("hidden");
 };
+
+const loadModal =async(id)=>{
+  const url=`https://openapi.programming-hero.com/api/plant/${id}`
+  const res = await fetch(url)
+  const data = await res.json()
+  displayModal(data.plants)
+}
+
+const displayModal =(details)=>{
+  modalContainer.innerHTML=`
+  <h2 class="card-title text-green-900 font-bold text-3xl" >${details.name}</h2>
+ <img class="h-48 w-full object-cover  rounded-sm my-2" src="${details.image}" alt="">
+ 
+ <p class="text-gray-500 font-bold text-2xl">Category: <span class="badge badge-outline bg-purple-950 text-gray-300">${details.category}</span></p>
+  <p class="line-clamp-2 text-gray-500 " >${details.description}</p>
+  <h3 class="text-3xl font-bold text-green-400 cursor-pointer" >$${details.price}</h3>
+  `
+
+
+document.getElementById('modal').showModal()
+
+}
 
 const catagoriesBth = () => {
   const url = "https://openapi.programming-hero.com/api/categories";
@@ -29,19 +53,30 @@ const displayCatagoriesBth = (buttons) => {
 };
 
 const selectCategory = (categoryId) => {
-showLoading()
+
   const allCategoriesBtn = document.querySelectorAll(
     "#Categories-container button, #trees-btn"
   );
+  showLoading()
 // console.log(allCategoriesBtn)
   allCategoriesBtn.forEach((button) => {
     button.classList.remove("text-white", "bg-green-400");
+    button.classList.add("btn-outline");
   });
 
   const clickedBtn = event.currentTarget;
   clickedBtn.classList.add("text-white", "bg-green-400");
-  console.log(clickedBtn)
+  // console.log(clickedBtn)
+  const url =`https://openapi.programming-hero.com/api/category/${categoryId}`
+  fetch(url)
+  .then((res)=>res.json())
+  .then((data)=>{
+    displayPlant(data.plants)
+  hideLoading()})
+  
 };
+
+
 
 const loadPlant = () => {
   showLoading();
@@ -66,14 +101,15 @@ const displayPlant = (allplants) => {
      src="${plant.image}"
      alt="${plant.name}"
      title="${plant.name}"
-     class="h-48 w-full object-cover"
+     class="h-48 w-full object-cover cursor-pointer"
+     onclick="loadModal(${plant.id})"
        />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">${plant.name}</h2>
-    <p class="line-clamp-2">${plant.description}</p>
-    <div class="badge badge-outline badge-success">${plant.category}</div>
-    <div class="card-actions justify-between"> <h3 class="text-xl font-bold text-green-400">$ ${plant.price}</h3>
+  <div   class="card-body">
+    <h2 class="card-title cursor-pointer" onclick="loadModal(${plant.id})">${plant.name}</h2>
+    <p class="line-clamp-2 cursor-pointer" onclick="loadModal(${plant.id})">${plant.description}</p>
+    <div  class="badge badge-outline badge-success border border-green-400 cursor-pointer" onclick="loadModal(${plant.id})">${plant.category}</div>
+    <div class="card-actions justify-between"> <h3 class="text-xl font-bold text-green-400 cursor-pointer" onclick="loadModal(${plant.id})" >$ ${plant.price}</h3>
       <button class="btn bg-green-400 text-white ">Cart</button>
     </div>
   </div>
